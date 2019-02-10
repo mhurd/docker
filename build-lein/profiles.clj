ad509cc61943cda99d4bb3a1c7a0e1cfb35141a4
Mike Hurd <mhurd@users.noreply.github.com>
2019-02-10 14:35:01 +0000

    Delete profiles.clj

--- build-lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:mirrors {#".+" {:url "http://nexus.home:8082/repository/maven-group/"}}
-        :repositories [["snapshots" {:id "neux-home-snapshots" 
-                                     :url "http://nexus.home:8082/repository/maven-snapshots"}]
-                       ["releases" {:id "nexus-home-releases"
-                                    :url "http://nexus.example.com:8081/nexus/content/repositories/releases" 
-                                    :sign-releases false}]]}
-  :auth {:repository-auth {#"nexus.home" {:username "jenkins-build" :password "jenkins-build"}}}}
